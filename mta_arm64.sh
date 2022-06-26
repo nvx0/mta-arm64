fabbebe0a073ad5c89232f0c7ef452e6b1090278
Jakub Kun <80093182+nvx0@users.noreply.github.com>
2022-06-26 10:52:34 +0200

    Update mta_arm64.sh

--- mta_arm64.sh
+++ mta_arm64.sh
@@ -3,6 +3,7 @@ echo "------ installing mta arm64 server ------"
 echo "[0/6] downloading core.."
 echo "[1/6] updating.."
 sudo apt install wget -y
+sudo apt install firewalld -y
 echo "[2/6] done updating"
 wget https://linux.multitheftauto.com/dl/multitheftauto_linux_arm64.tar.gz
 echo "[3/6] done downloading core"
@@ -11,7 +12,10 @@ tar -xf multitheftauto_linux_arm64.tar.gz
 rm -f baseconfig.tar.gz
 echo "[5/6] downloading config.."
 wget https://linux.mtasa.com/dl/baseconfig.tar.gz
-echo "[6/6] unpacking everything.."
+echo "[6/6] getting everything done.."
+sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --add-service=https --permanent
+sudo firewall-cmd --reload
 tar -xf baseconfig.tar.gz
 mv baseconfig/* multitheftauto_linux_arm64/mods/deathmatch
 echo "------ installing mta arm64 server ------"
